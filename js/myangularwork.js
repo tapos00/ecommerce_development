70cae9b0602cad6886f9aebe6cd9b38e1f88f11f
tapos <tapos@r-cis.com.com>
2015-11-27 13:17:23 +0600

    ecommerce first class done

---
+++ js/myangularwork.js
@@ -0,0 +1,41 @@
+(function(){
+    var app = angular.module("myApp",[]);
+
+    app.filter('uniqueCategory',function(){
+       return function(allproduct) {
+           if(angular.isArray(allproduct)){
+               var cat = {};
+               var catlist  = [];
+               angular.forEach(allproduct, function(aproduct) {
+                   var nowcat = aproduct.category;
+                   if(angular.isUndefined(cat[nowcat])){
+                       cat[nowcat] = true;
+                       catlist.push(nowcat);
+                       console.log(cat);
+                   }
+               });
+               return catlist;
+           }
+           return allproduct;
+       }
+    });
+
+    app.controller("myCtrl",['$scope','$http',function($scope,$http){
+        $scope.productList = [];
+        $scope.selectpro = null;
+        $http.get('/productlist.json').then(function(response){
+            $scope.productList = response.data;
+        }, function(){
+
+        });
+        $scope.seletedProduct = function(aCategory){
+            $scope.selectpro = aCategory;
+        };
+        $scope.clicktoselect = function(aproduct){
+            return $scope.selectpro ==null || $scope.selectpro == aproduct.category;
+        };
+        $scope.selectactive = function(category){
+            return category==$scope.selectpro ? "active":null;
+        }
+    }]);
+})();
---
+++ js/test.js
@@ -0,0 +1,6 @@
+var cat = {
+    soccer : true,
+};
+var categorylist = [soccer,];
+cat.name = "sumon";
+if(cat.roll)
